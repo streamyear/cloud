4df10d27bbff2558a74ab032cfcca79cf229ad2e
streamyear <34082562+streamyear@users.noreply.github.com>
2021-01-09 20:47:05 +0800

    eureka finish

--- cloud-eureka-server7001/src/main/java/com/greateforest/cloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/greateforest/cloud/EurekaMain7001.java
@@ -1,14 +1,13 @@
 package com.greateforest.cloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class OrderMain80 {
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
--- cloud-provider-payment8002/src/main/java/com/greateforest/cloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/greateforest/cloud/controller/PaymentController.java
@@ -6,11 +6,7 @@ import com.greateforest.cloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @Slf4j
@@ -21,9 +17,6 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -43,18 +36,5 @@ public class PaymentController {
             return new CommonResult(200,"查询成功, serverPort: " + serverPort,payment);
         }
         return new CommonResult(404,"没有对应记录，查询ID："+id,null);
-    }
-
-    @GetMapping(value = "/payment/discovery")
-    public Object discovery(){
-        List<String> services = discoveryClient.getServices();
-        for (String element : services) {
-            log.info("***** element:"+element);
-        }
-        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-        for (ServiceInstance instance : instances) {
-            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
-        }
-        return this.discoveryClient;
     }
 }
