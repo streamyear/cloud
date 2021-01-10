ff1e072d82dbc54ac3331e81c286e79a0689ea54
streamyear <34082562+streamyear@users.noreply.github.com>
2021-01-10 10:27:57 +0800

    zookeeper finish

---
+++ cloud-consumerzk-order80/src/main/java/com/greateforest/controller/OrderZKController.java
@@ -0,0 +1,23 @@
+package com.greateforest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderZKController {
+
+    public static final String INVOME_URL = "http://cloud-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zk")
+    public String payment() {
+        String result = restTemplate.getForObject(INVOME_URL + "/payment/zk", String.class);
+        return result;
+    }
+}
